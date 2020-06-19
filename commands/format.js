278844f517091c227947ad22218a5740a005f9cb
Bob <albraveraphael@gmail.com>
2020-06-19 19:53:17 +0800

    test to australs

---
+++ commands/format.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name: "format",
+  description:
+    "Sets a motion then starts a prep timer -- exclusive for runner1",
+  execute(state, message, args) {
+    const Discord = require("discord.js");
+    const timer = require("./timer.js");
+
+    // There has to be a format as an argument
+    if (args.length < 1) {
+      message.channel.send(
+        "Imprope format. Must follow example: `-format Australs` or `-format BP`."
+      );
+      return;
+    }
+
+    // Change format
+    const format = args.join(" ");
+    state.format = format;
+    console.log(`Format set to ${format}`);
+  }
+};
---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  token: "NzEzMzQwMjc4MDUxOTYyOTEw.XuJzww.s-gUhIJeX0UWEx442MyVYO3I7rY"
+};
