d2a075e021ab44d38e4bf7b24d96dc048a5abced
Bob <albraveraphael@gmail.com>
2020-06-25 18:24:18 +0800

    fixed bug in exit.js

--- commands/exit.js
+++ commands/exit.js
@@ -3,7 +3,7 @@ module.exports = {
   description: "Bot leaves voice channel",
   execute(state, message, args) {
     if (state.connection) {
-      message.channel.send(runner + " has disconnected from " + state.voiceChannel);
+      message.channel.send(state.runner + " has disconnected from " + state.voiceChannel);
       state.connection.disconnect();
     }
   }
