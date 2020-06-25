1a27e730046a0ec99fbf9376ef427c4f88ec7965
Bob <albraveraphael@gmail.com>
2020-06-25 18:16:07 +0800

    removed test for inVoice

--- commands/exit.js
+++ commands/exit.js
@@ -2,10 +2,9 @@ module.exports = {
   name: "exit",
   description: "Bot leaves voice channel",
   execute(state, message, args) {
-    if (state.connection && state.inVoice) {
+    if (state.connection) {
       message.channel.send(runner + " has disconnected from " + state.voiceChannel);
       state.connection.disconnect();
-      state.inVoice = false;
     }
   }
 };
